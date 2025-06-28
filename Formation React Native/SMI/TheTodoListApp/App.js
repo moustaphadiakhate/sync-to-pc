5d22912e1133e99572df5081efd4698133436da0
Moustapha <moustaphadiakhate1212@gmail.com>
2025-06-28 12:41:24 +0000

    [00]

--- Formation React Native/SMI/TheTodoListApp/App.js
+++ Formation React Native/SMI/TheTodoListApp/App.js
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons';
 import ToDoListScreen from './src/screens/TodoList';
 import DetailScreen from './src/screens/Detail';
 import ProfileScreen from "./src/screens/Profile";
+
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
@@ -27,8 +29,21 @@ function TodoNavigation() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Profile") {
+              iconName = "person";
+            } else if (route.name === "Todo") {
+              iconName = "list";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Todo" component={TodoNavigation} />
       </Tab.Navigator>
     </NavigationContainer>
   );
