7cef1354cc4ecf64c57a614ac0dbac66bb938b83
Moustapha <moustaphadiakhate1212@gmail.com>
2025-06-28 10:43:24 +0000

    [00]

--- Formation React Native/SMI/TheTodoListApp/App.js
+++ Formation React Native/SMI/TheTodoListApp/App.js
@@ -1,25 +1,35 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ToDoListScreen from './screens/ToDoListScreen';
-import DetailScreen from './screens/DetailScreen';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ToDoListScreen from './src/screens/TodoList';
+import DetailScreen from './src/screens/Detail';
+import ProfileScreen from "./src/screens/Profile";
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TodoNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="TodoList"
+        component={ToDoListScreen}
+        options={{ title: 'Todo Page' }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailScreen}
+        options={{ title: 'Détail de la tâche' }}
+      />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="TodoList"
-          component={ToDoListScreen}
-          options={{ title: 'Todo Page' }}
-        />
-        <Stack.Screen
-          name="Detail"
-          component={DetailScreen}
-          options={{ title: 'Détail de la tâche' }}
-        />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
