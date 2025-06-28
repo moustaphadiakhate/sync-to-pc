ea7748982c556a350668405cdcd5510aaf008bbf
Moustapha <moustaphadiakhate1212@gmail.com>
2025-06-28 13:13:13 +0000

    [00]

--- Formation React Native/SMI/TheTodoListApp/App.js
+++ Formation React Native/SMI/TheTodoListApp/App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 import ToDoListScreen from './src/screens/TodoList';
 import DetailScreen from './src/screens/Detail';
 import ProfileScreen from "./src/screens/Profile";
